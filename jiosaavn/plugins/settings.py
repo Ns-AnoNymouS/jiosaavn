77b6d13183a3def928316ad0491ec00951ebdf5d
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-06-28 12:24:26 +0530

    Update settings.py

--- jiosaavn/plugins/settings.py
+++ jiosaavn/plugins/settings.py
@@ -4,4 +4,5 @@ from ..tools.request import req
 
 @Client.on_callback_query(filters.command('settings'))
 async def settings(c, m):
-    await m.reply_text()
+
+    await m.reply_text('select the search result type', reply_markup=InlineKeyboardMarkup())
