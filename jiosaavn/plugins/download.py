beb62370ba6dd8727ea70d3d0b26a70cdfde2e57
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-06-28 16:47:33 +0530

    Update download.py

--- jiosaavn/plugins/download.py
+++ jiosaavn/plugins/download.py
@@ -5,6 +5,13 @@ from pyrogram import Client, filters
 async def download(c, m, cb=False):
     if not cb:
         send_msg = await m.reply_text('Checking...🕵‍♂️', quote=True)
+        if 'jiosaavn' not in m.text:
+            await send_msg.edit('__Currently only jiosaavn links are supported 🤭__')
+        type = 'song' if 'song' in m.text else 'album'
+        id = m.text.split('/')[-1]
     else:
         send_msg = m.message
+        cmd, id, type = m.data.split('+')
+
+    
 
