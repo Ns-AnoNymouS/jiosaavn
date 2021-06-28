5e277b5078e333f8a4763ac773f10f12cc974f28
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-06-28 19:42:04 +0530

    Update database.py

--- jiosaavn/database/database.py
+++ jiosaavn/database/database.py
@@ -4,10 +4,12 @@ import motor.motor_asyncio
  
 class Database:
     
-    def __init__(self, uri, database_name):
+    def __init__(self, uri):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
-        self.db = self._client[database_name]
-        self.col = self.db.users
+        self.user_db = self._client['jiosaavn_users']
+        self.id_db = self._client['jiosaavn_ids']
+        self.col = self.user_db.users
+        self.
     
     def new_user(self, id):
         return dict(
