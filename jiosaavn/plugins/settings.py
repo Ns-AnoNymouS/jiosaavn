9b2d7a352bfecad46583803185f512bae148f5de
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-06-28 12:21:41 +0530

    Update settings.py

--- jiosaavn/plugins/settings.py
+++ jiosaavn/plugins/settings.py
@@ -1,1 +1,6 @@
 from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from ..tools.request import req
+
+@Client.on_callback_query(filters.command('settings'))
+async def settings(c, m):
