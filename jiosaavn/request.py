91969062a6d277b58adf243509a8d7f1fc424836
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-06-28 07:32:51 +0530

    Create request.py

---
+++ jiosaavn/request.py
@@ -0,0 +1,7 @@
+import aiohttp
+
+async def req(url, params):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=params, raise_for_status=True) as response:
+            data = await response.json()
+            return data
