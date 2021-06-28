c5b20333b348f63da412a6933016af0f5db3dfda
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-06-28 07:47:05 +0530

    Update search.py

--- jiosaavn/plugins/search.py
+++ jiosaavn/plugins/search.py
@@ -2,10 +2,20 @@ from pyrogram import Client, filters
 from ..tools.request import req
 
 
-api_url = 'https://www.jiosaavn.com/api.php?'
 @Client.on_message(filters.text & filters.incoming & filters.private & ~)
 async def search(c, m):
     send_msg = await m.reply_text('**Processing... ⏳**', quote=True)
 
-    
-    data = await req()
+    api_url = 'https://www.jiosaavn.com/api.php?'
+    params = {
+        'p': 1,
+        'q': m.text,
+        '_format': 'json',
+        '_marker': 0,
+        'api_version': 4,
+        'ctx': 'wap6dot0',
+        'n': 10,
+        '__call': 'search.getAlbumResults'
+    }
+    data = await req(api_url, params)
+    print(data)
