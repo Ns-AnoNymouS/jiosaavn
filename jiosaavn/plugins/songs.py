5ce8ee71b9f1f5592816548d5f3138daa944d975
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-06-28 14:33:45 +0530

    Update songs.py

--- jiosaavn/plugins/songs.py
+++ jiosaavn/plugins/songs.py
@@ -3,4 +3,12 @@ from pyrogram import Client, filters
 
 @Client.on_callback_query(filters.regex('^open$'))
 async def opensong(c, m):
+    url = 'https://www.jiosaavn.com/api.php?'
+    params = {
+        '__call': 'song.getDetails',
+        'cc': 'in',
+        '_marker': '0%3F_marker%3D0',
+        '_format': 'json',
+        'pids': 
+    }
     await m.edit()
