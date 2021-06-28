340fbcdef8f524a6c1e2b1fcbbb6d46e3ba7946e
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-06-28 15:54:26 +0530

    Update album.py

--- jiosaavn/plugins/album.py
+++ jiosaavn/plugins/album.py
@@ -6,4 +6,14 @@ from ..tools.request import req
 @Client.on_callback_query(filters.regex('^album\+'))
 async def openalbum(c, m):
     await m.answer()
-https://www.jiosaavn.com/api.php?__call=content.getAlbumDetails&_format=json&cc=in&_marker=0%3F_marker%3D0&albumid=
+
+    url = 'https://www.jiosaavn.com/api.php?'
+    params = {
+        '__call': 'content.getAlbumDetails',
+        'cc': 'in',
+        '_marker': '0%3F_marker%3D0',
+        '_format': 'json',
+        'albumid': song_id
+    }
+    data = await req(url, params)
+    print(data)
