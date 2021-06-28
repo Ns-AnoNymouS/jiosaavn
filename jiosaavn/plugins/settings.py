771d5b3c11cb968dd39efd34a3d4321c69ef7d81
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-06-28 12:25:04 +0530

    Update settings.py

--- jiosaavn/plugins/settings.py
+++ jiosaavn/plugins/settings.py
@@ -4,5 +4,7 @@ from ..tools.request import req
 
 @Client.on_callback_query(filters.command('settings'))
 async def settings(c, m):
+    if not await c.db.is_user_exist(m.from_user.id):
+        await c.db.add_user(m.from_user.id)
 
     await m.reply_text('select the search result type', reply_markup=InlineKeyboardMarkup())
