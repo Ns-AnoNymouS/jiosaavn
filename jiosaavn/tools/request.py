c01e821f40f5c352cd388805bc30ac9bb9aee853
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-06-28 10:16:07 +0530

    Update request.py

--- jiosaavn/tools/request.py
+++ jiosaavn/tools/request.py
@@ -6,7 +6,6 @@ async def req(url, params):
     async with aiohttp.ClientSession() as session:
         async with session.get(url, params=params) as response:
             data = await response.text()
-            data = data.encode().decode('unicode-escape')
-            jsonDump = json.dumps(data)
-            jsonResult = json.load(data)
+            data = data.encode().decode()
+            jsonResult = json.loads(data)
             return jsonResult
