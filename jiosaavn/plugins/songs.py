610ec6d37570026db62fe9a1fc2805a2b011f46a
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-06-28 12:48:52 +0530

    Update songs.py

--- jiosaavn/plugins/songs.py
+++ jiosaavn/plugins/songs.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 
 
-@Client.on_callback_query(filters.regex(''))
+@Client.on_callback_query(filters.regex('^open$'))
 async def opensong(c, m):
     await m.edit()
