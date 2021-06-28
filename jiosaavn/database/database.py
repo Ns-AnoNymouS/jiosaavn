efe04c601e19c55aa4c48f4e9da1283372fc7961
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-06-28 10:42:49 +0530

    Update database.py

--- jiosaavn/database/database.py
+++ jiosaavn/database/database.py
@@ -13,7 +13,7 @@ class Database:
         return dict(
             id = id,
             join_date = datetime.date.today().isoformat(),
-            settings="all",
+            type="all",
             ban_status={
                 'is_banned': False,
                 'ban_duration': 0,
@@ -22,15 +22,21 @@ class Database:
             }
         )
     
-
-################## Checking & Adding New User 👤 ##################
-
     async def is_user_exist(self, id):
         user = await self.col.find_one({'id':id})
         return True if user else False
-
 
     async def add_user(self, id):
         user = self.new_user(id)
         await self.col.insert_one(user)
     
+    async def get_type(self, id):
+        user = await self.col.find_one({'id':id})
+        type = user.get('type')
+        return type
+
+    async def update_type(self, id, value):
+        user = await self.col.find_one({'id':id})
+        type = user.get('type')
+        await self.col.update_one({'id': id}, {'$set': {'type': type}})   
+
