9d3da829688f76d83a4ad3db4833e3f052a1f95a
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-06-28 07:50:59 +0530

    Create database.py

---
+++ jiosaavn/database.py
@@ -0,0 +1,11 @@
+import datetime
+import motor.motor_asyncio
+ 
+ 
+class Database:
+    
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self._client[database_name]
+        self.col = self.db.users
+    
