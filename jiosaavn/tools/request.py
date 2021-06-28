1e98235dbbee53fe5ccd46f400d280a62e0378fc
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-06-28 08:25:27 +0530

    Update request.py

--- jiosaavn/tools/request.py
+++ jiosaavn/tools/request.py
@@ -3,13 +3,6 @@ import aiohttp
 
 
 async def req(url, params):
-    useragent = ('Mozilla/5.0 (Linux; Android 10; SM-G975F) '
-                 'AppleWebKit/537.36 (KHTML, like Gecko) '
-                 'Chrome/80.0.3987.149 Mobile Safari/537.36')
-    headers = {
-        'User-Agent': useragent,
-        'Accept': 'application/json',
-    }
     async with aiohttp.ClientSession() as session:
         async with session.get(url, params=params) as response:
             data = await response.text()
