6054afbebe117b3fc34a24c62e26bae9e02db53a
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-06-29 14:24:53 +0530

    Update Inline.py

--- jiosaavn/plugins/Inline.py
+++ jiosaavn/plugins/Inline.py
@@ -1,1 +1,3 @@
-
+from pyrogram import Client, filters
+from pyrogram.types import InlineQueryResultArticle, InlineQueryResultPhoto, InlineKeyboardMarkup, InlineKeyboardButton, InputTextMessageContent, ChosenInlineResult, InputMediaAudio    
+from ..tools.request import req
