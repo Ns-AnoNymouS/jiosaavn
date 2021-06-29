9e07863e5cbbfceae1727fbd7da954afb8fb1ed5
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-06-29 13:14:23 +0530

    Update upload_helpers.py

--- jiosaavn/tools/upload_helpers.py
+++ jiosaavn/tools/upload_helpers.py
@@ -1,1 +1,19 @@
-
+async def send_audio(c, chat_id, audio, caption, duration, title, thumb, artists, reply_to_message_id):
+    try:
+        song = await c.send_audio(
+            chat_id=chat_id,
+            audio=audio,
+            caption=caption,
+            duration=duration,
+            title=title,
+            thumb=thumb,
+            performer=artists,
+            parse_mode="markdown",
+            reply_to_message_id=reply_to_message_id
+        )
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        await send_audio(c, chat_id, audio, caption, duration, title, thumb, artists, reply_to_message_id)
+    except Exception as e:
+        logger.warning(e)
+    
