e73c33ca32a8a36367babcbcc4f700064fdae90b
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-06-29 14:33:47 +0530

    Update Inline.py

--- jiosaavn/plugins/Inline.py
+++ jiosaavn/plugins/Inline.py
@@ -1,3 +1,27 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineQueryResultArticle, InlineQueryResultPhoto, InlineKeyboardMarkup, InlineKeyboardButton, InputTextMessageContent, ChosenInlineResult, InputMediaAudio    
 from ..tools.request import req
+
+
+@Client.on_inline_query()
+async def search_inline(c, m):
+    if m.query == '':
+        await m.answer(
+            results=[],
+            cache_time=0,
+            switch_pm_text=f"🔎 Type the song name for searching...",
+            switch_pm_parameter="help",
+        )
+        return
+
+    if m.query == 'Album: '
+        await m.answer(
+            results=[],
+            cache_time=0,
+            switch_pm_text=f"🔎 Type the Album name for searching...",
+            switch_pm_parameter="help",
+        )
+        return
+
+    offset = m.offset if m.offset else 0
+    
