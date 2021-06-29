7dd84047a105a56341e1af9512dff77ce370222b
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-06-29 08:13:40 +0530

    Update download.py

--- jiosaavn/plugins/download.py
+++ jiosaavn/plugins/download.py
@@ -78,11 +78,11 @@ async def download_tool(c, m, id, reply_to_message_id, msg):
 
     await msg.edit(f'__📥 Downloading {song}__')
     async with aiohttp.ClientSession() as session: 
-        async with session.get(url) as response:
+        async with session.get(url, stream=True) as response:
             with open(file_name, "wb") as file:
                 while True:
                     try:
-                        chunk = await response.content.read(128)
+                        chunk = await response.content.read(4 * 1024 * 1024)
                     except:
                         break
                     if not chunk:
@@ -94,7 +94,7 @@ async def download_tool(c, m, id, reply_to_message_id, msg):
             with open(thumbnail_location, "wb") as file:
                 while True:
                     try:
-                        chunk = await response.content.read(128)
+                        chunk = await response.content.read(4 * 1024 * 1024)
                     except:
                         break
                     if not chunk:
