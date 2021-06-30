6009aa4bb5539c568b814143b534aac78297ec44
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-06-30 12:29:52 +0530

    Update xsearch.py

--- jiosaavn/plugins/xsearch.py
+++ jiosaavn/plugins/xsearch.py
@@ -138,15 +138,21 @@ async def nxt_cb(c, m):
     if type != 'all':
         total_results = data['total']
         for result in data['results']:
-            title = result['title'] if 'title' in result else ''
             id = result['id'] if 'id' in result else None
-            if result['type'] == 'song':
-                album = ''
-                if 'more_info' in result:
-                    album = result['more_info']['album'] if 'album' in result['more_info'] else ''
-                buttons.append([InlineKeyboardButton(f"🎙 {title} from '{album}'", callback_data=f'open+{id}')])
-            elif result['type'] == 'album':
-                buttons.append([InlineKeyboardButton(f"📚 {title}", callback_data=f'album+{id}')])
+            if 'type' in result:
+                title = result['title'] if 'title' in result else ''
+                if result['type'] == 'song':
+                    album = ''
+                    if 'more_info' in result:
+                        album = result['more_info']['album'] if 'album' in result['more_info'] else ''
+                    buttons.append([InlineKeyboardButton(f"🎙 {title} from '{album}'", callback_data=f'open+{id}')])
+                elif result['type'] == 'album':
+                    buttons.append([InlineKeyboardButton(f"📚 {title}", callback_data=f'album+{id}')])
+                elif result['type'] == 'playlist':
+                    buttons.append([InlineKeyboardButton(f"💾 {title}", callback_data=f'playlist+{id}')])
+            else:
+                buttons.append([InlineKeyboardButton(f"👨‍🎤 {result['name']}", callback_data=f'artist+{id}')])
+
     else:
         index_btn = []
         for album in data['albums']['data']:
