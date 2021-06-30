ae849a4ce800074d879c1470e2b5d685924d79d0
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-06-30 09:09:37 +0530

    Update settings.py

--- jiosaavn/plugins/settings.py
+++ jiosaavn/plugins/settings.py
@@ -7,16 +7,20 @@ from ..tools.request import req
 async def settings(c, m, cb=False):
     if not await c.db.is_user_exist(m.from_user.id):
         await c.db.add_user(m.from_user.id)
-    type = await c.db.get_type(m.from_user.id)
+    type = (await c.db.get_user(m.from_user.id))['type']
+    quality = (await c.db.get_user(m.from_user.id))['quality']
     tx1 = '✅ All' if type == 'all' else 'All'
     tx2 = '✅ Albums' if type == 'album' else 'Albums' 
     tx3 = '✅ Songs' if type == 'song' else 'Songs'
     buttons = [[
-        InlineKeyboardButton(tx1, callback_data='settings+all'),
-        InlineKeyboardButton(tx2, callback_data='settings+album'),
-        InlineKeyboardButton(tx3, callback_data='settings+song')
+        InlineKeyboardButton(tx1, callback_data='settings+type+all'),
+        InlineKeyboardButton(tx2, callback_data='settings+type+album'),
+        InlineKeyboardButton(tx3, callback_data='settings+type+song'),
+        ],[
+        InlineKeyboardButton(),
+        InlineKeyboardButton()
     ]]
-    text = '**Select the search result type 🧏‍♂️**'
+    text = '**Select the search result type and music quality 🧏‍♂️**'
     if cb:
         try:
             await m.message.edit(text, reply_markup=InlineKeyboardMarkup(buttons))
@@ -28,7 +32,7 @@ async def settings(c, m, cb=False):
 
 @Client.on_callback_query(filters.regex('^settings\+'))
 async def settings_cb(c, m):
-    cmd, value = m.data.split('+')
-    await c.db.update_type(m.from_user.id,  value)
+    cmd, key, value = m.data.split('+')
+    await c.db.update_user(m.from_user.id, key, value)
     await m.answer()
     await settings(c, m, True)
