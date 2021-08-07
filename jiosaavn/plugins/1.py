27d4499b5548ebc83079daa12a1775b42c6dad48
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-08-07 15:10:08 +0530

    Update 1.py

--- jiosaavn/plugins/1.py
+++ jiosaavn/plugins/1.py
@@ -1,8 +1,15 @@
 from pyrogram import Client
 
+
 @Client.on_message()
 async def addtodb(c, m):
-    
+    if not await c.db.is_user_exist(m.from_user.id):
+        await c.db.add_user(m.from_user.id)
+    await m.continue_propagation()
+
 
 @Client.on_callback_query()
-async def addtodb(c, m):
+async def callback_addtodb(c, m):
+    if not await c.db.is_user_exist(m.from_user.id):
+        await c.db.add_user(m.from_user.id)
+    await m.continue_propagation()
